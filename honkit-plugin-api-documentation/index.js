3c7c31d83ba5f7e5e733307ad1b941793c84529e
breadpitt <pascal.witzig@gmail.com>
2023-11-20 02:47:55 +0100

    Attempt at implementing swagger-parser

--- honkit-plugin-api-documentation/index.js
+++ honkit-plugin-api-documentation/index.js
@@ -1,12 +1,12 @@
-const fs = require('fs');
-const Handlebars = require('handlebars');
-const path = require('path');
+import { readFileSync, existsSync, writeFileSync, appendFileSync } from 'fs';
+import { registerHelper, helpers, SafeString, compile } from 'handlebars';
+import { join } from 'path';
 
 // Read and compile the template
-const templateSource = fs.readFileSync(path.join(__dirname, '_layouts', 'dynamic_template.hbs'), 'utf8');
+const templateSource = readFileSync(join(__dirname, '_layouts', 'dynamic_template.hbs'), 'utf8');
 console.log("Template Source:", templateSource);
 
-Handlebars.registerHelper('renderProperties', function(properties, options) {
+registerHelper('renderProperties', function(properties, options) {
   if (!properties || !Array.isArray(properties)) {
     return ''; // Early exit
   }
@@ -22,7 +22,7 @@ Handlebars.registerHelper('renderProperties', function(properties, options) {
       output += `<details open=""><summary><span class="title">${property.title}${requiredFlag}</span><span class="type">${property.type}</span></summary>`;
       output += `<div class="nested">`;
       // Explicitly invoking the helper with the new context
-      output += Handlebars.helpers.renderProperties(property.properties, options);
+      output += helpers.renderProperties(property.properties, options);
       output += `</div></details>`;
     } else if (property.type.startsWith('array') && property.properties) {
       output += `<details open=""><summary><span class="title">${property.title}${requiredFlag}</span><span class="type">${property.type}</span></summary>`;
@@ -35,7 +35,7 @@ Handlebars.registerHelper('renderProperties', function(properties, options) {
     }
   }
 
-  return new Handlebars.SafeString(output);
+  return new SafeString(output);
 });
 
 
@@ -43,81 +43,79 @@ Handlebars.registerHelper('renderProperties', function(properties, options) {
 
 
 
-const template = Handlebars.compile(templateSource);
+const template = compile(templateSource);
 
-module.exports = {
-  book: {
-    assets: './assets',
-    js: ['js/formHandler.js'],
-    css: ['style.css']
-  },
-  hooks: {
-  "init": function() {
-            // Paths for the new Markdown file and SUMMARY.md
-            const mdFilePath = path.join(this.resolve(''), 'jsonCreator.md');
-            const summaryPath = path.join(this.resolve(''), 'SUMMARY.md');
+export const book = {
+  assets: './assets',
+  js: ['js/formHandler.js', ],
+  css: ['style.css']
+};
+export const hooks = {
+  "init": function () {
+    // Paths for the new Markdown file and SUMMARY.md
+    const mdFilePath = join(this.resolve(''), 'jsonCreator.md');
+    const summaryPath = join(this.resolve(''), 'SUMMARY.md');
 
-            // Content for the new Markdown file
-            const mdContent = '# JSON Creator\n\n{% include "./honkit-plugin-api-documentation/_layouts/form.hbs" %}';
-            const summaryLink = '\n* [JSON Creator](jsonCreator.md)';
+    // Content for the new Markdown file
+    const mdContent = '# JSON Creator\n\n{% include "./honkit-plugin-api-documentation/_layouts/form.hbs" %}';
+    const summaryLink = '\n* [JSON Creator](jsonCreator.md)';
 
-            // Check if the Markdown file already exists
-            if (!fs.existsSync(mdFilePath)) {
-                fs.writeFileSync(mdFilePath, mdContent);
-            }
+    // Check if the Markdown file already exists
+    if (!existsSync(mdFilePath)) {
+      writeFileSync(mdFilePath, mdContent);
+    }
 
-            // Check if the link is already in SUMMARY.md
-            const summaryContent = fs.readFileSync(summaryPath, 'utf-8');
-            if (!summaryContent.includes(summaryLink.trim())) {
-                fs.appendFileSync(summaryPath, summaryLink);
-            }
-        },
-    'page:before': async function(page) {
-      const markerStart = '<!-- DYNAMIC_TEMPLATE_START -->';
-      const markerEnd = '<!-- DYNAMIC_TEMPLATE_END -->';
-      let content = page.content;
-
-      while (content.indexOf(markerStart) !== -1) {
-        const start = content.indexOf(markerStart);
-        const end = content.indexOf(markerEnd, start);
-
-        if (end === -1) {
-          break;
-        }
-
-        const beforeMarker = content.slice(0, start);
-        const afterMarker = content.slice(end + markerEnd.length);
-        const markerContent = content.slice(start + markerStart.length, end);
-
-        let data;
-        try {
-        console.log(markerContent.trim())
-          data = JSON.parse(markerContent.trim());
-        } catch (error) {
-          console.error('Error parsing JSON for dynamic template:', error);
-          break;
-        }
-
-        const dynamicContent = template({
-          title: data.title,
-          method: data.method,
-          baseUrl: data.baseUrl,
-          path: data.path,
-          apiTitle: data.apiTitle,
-          description: data.description,
-          paramName: data.paramName,
-          required: data.required,
-          type: data.type,
-          paramDescription: data.paramDescription,
-          responses: data.responses  // Assume this is an array of objects
-        });
-
-        content = beforeMarker + dynamicContent + afterMarker;
-      }
-
-      page.content = content;
-      return page;
+    // Check if the link is already in SUMMARY.md
+    const summaryContent = readFileSync(summaryPath, 'utf-8');
+    if (!summaryContent.includes(summaryLink.trim())) {
+      appendFileSync(summaryPath, summaryLink);
     }
   },
-  filters: {}
+  'page:before': async function (page) {
+    const markerStart = '<!-- DYNAMIC_TEMPLATE_START -->';
+    const markerEnd = '<!-- DYNAMIC_TEMPLATE_END -->';
+    let content = page.content;
+
+    while (content.indexOf(markerStart) !== -1) {
+      const start = content.indexOf(markerStart);
+      const end = content.indexOf(markerEnd, start);
+
+      if (end === -1) {
+        break;
+      }
+
+      const beforeMarker = content.slice(0, start);
+      const afterMarker = content.slice(end + markerEnd.length);
+      const markerContent = content.slice(start + markerStart.length, end);
+
+      let data;
+      try {
+        console.log(markerContent.trim());
+        data = JSON.parse(markerContent.trim());
+      } catch (error) {
+        console.error('Error parsing JSON for dynamic template:', error);
+        break;
+      }
+
+      const dynamicContent = template({
+        title: data.title,
+        method: data.method,
+        baseUrl: data.baseUrl,
+        path: data.path,
+        apiTitle: data.apiTitle,
+        description: data.description,
+        paramName: data.paramName,
+        required: data.required,
+        type: data.type,
+        paramDescription: data.paramDescription,
+        responses: data.responses // Assume this is an array of objects
+      });
+
+      content = beforeMarker + dynamicContent + afterMarker;
+    }
+
+    page.content = content;
+    return page;
+  }
 };
+export const filters = {};
